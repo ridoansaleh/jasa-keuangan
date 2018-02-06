4e653ad9d219f1c4155f93fa411c4912d4b07280
ridoansaleh <ridoansalehnasution98@gmail.com>
2018-02-07 00:14:48 +0700

    add all files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jasa-keuangan
+
+### Built using these tools :
+
+1. ReactJs v16.2.0
+
+2. Bootstrap v4.0
+
+3. Font Awesome v4.7.0
---
+++ src/components/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import ProductDetail from './ProductDetail';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/font-awesome/css/font-awesome.min.css'; 
+import '../assets/Custom.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { show : 0 };
+    this.changeProductDetail = this.changeProductDetail.bind(this);
+  }
+
+  changeProductDetail(e, data) {
+    e.preventDefault();
+    this.setState({
+      show: data
+    });
+  }
+
+
+  render() {
+    const green = {borderBottom: '4px solid #42b549'};
+    const grey = {borderBottom: '4px solid #c7c7c7'};
+    return (
+      <div className="container">
+        <p className="app-title">Ajukan Aplikasi Produk Keuangan</p>
+        <div className="row justify-content-center">
+            <div className="box" style={this.state.show===1 ? green : grey} onClick={(e) => this.changeProductDetail(e, 1)}>
+              <div>
+                <i className="wrapper-icon box-icon mitratopper-icon"></i>
+              </div>
+              <p className="box-title">Mitra Toppers</p>
+              <p className="box-content">Mitra Penyedia Pinjaman Merchant Tokopedia</p>
+              <p className="box-detail">Lihat Detail Produk</p>
+            </div>
+            <div className="box" style={this.state.show===2 ? green : grey} onClick={(e) => this.changeProductDetail(e, 2)}>
+              <div>
+                <i className="wrapper-icon box-icon kartukredit-icon"></i>
+              </div>
+              <p className="box-title">Kartu Kredit</p>
+              <p className="box-content">Ajukan Kartu Kredit yang sesuai dengan kebutuhan</p>
+              <p className="box-detail">Lihat Detail Produk</p>
+            </div>
+            <div className="box" style={this.state.show===3 ? green : grey} onClick={(e) => this.changeProductDetail(e, 3)}>
+              <div>
+                <i className="wrapper-icon box-icon pinjamanonline-icon"></i>
+              </div>
+              <p className="box-title">Pinjaman Online</p>
+              <p className="box-content">Ajukan Pinjaman tanpa Agunan yang Sesuai</p>
+              <p className="box-detail">Lihat Detail Produk</p>
+            </div>
+            <div className="box" style={this.state.show===4 ? green : grey} onClick={(e) => this.changeProductDetail(e, 4)}>
+              <div>
+                <i className="wrapper-icon box-icon asuransi-icon"></i>
+              </div>
+              <p className="box-title">Asuransi</p>
+              <p className="box-content">Ajukan Asuransi yang sesuai dengan kebutuhan</p>
+              <p className="box-detail">Lihat Detail Produk</p>
+            </div>
+        </div>
+
+        <ProductDetail isComponent={this.state.show}/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
