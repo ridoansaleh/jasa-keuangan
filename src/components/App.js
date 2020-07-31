a9b3270ebfc5fd8e8f9caeeb9028a0baf7cfa885
RSN <ridoansalehnasution98@gmail.com>
2020-07-31 21:05:58 +0700

    add deployment script (#1)

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,111 @@
-import React, { Component } from 'react';
-import ProductDetail from './ProductDetail';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../../node_modules/font-awesome/css/font-awesome.min.css'; 
-import '../assets/Custom.css';
+import React, { Component } from "react";
+import ProductDetail from "./ProductDetail";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../node_modules/font-awesome/css/font-awesome.min.css";
+import "../assets/Custom.css";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { show : 0 };
+    this.state = { show: 0 };
     this.changeProductDetail = this.changeProductDetail.bind(this);
   }
 
   changeProductDetail(e, data) {
     e.preventDefault();
     this.setState({
-      show: data
+      show: data,
     });
   }
 
-
   render() {
-    const greenBorder = {borderBottom: '4px solid #42b549'};
-    const greyBorder = {borderBottom: '4px solid #c7c7c7'};
-    const greenColor = {color:'#42b549'};
-    const greyColor =  {color:'#c7c7c7'};
+    const greenBorder = { borderBottom: "4px solid #42b549" };
+    const greyBorder = { borderBottom: "4px solid #c7c7c7" };
+    const greenColor = { color: "#42b549" };
+    const greyColor = { color: "#c7c7c7" };
     return (
       <div className="container">
         <p className="app-title">Ajukan Aplikasi Produk Keuangan</p>
         <div className="row justify-content-center">
-            <div className="box" style={this.state.show===1 ? greenBorder : greyBorder} onClick={(e) => this.changeProductDetail(e, 1)}>
-              <div>
-                <i className="wrapper-icon box-icon mitratopper-icon"></i>
-              </div>
-              <p className="box-title">Mitra Toppers</p>
-              <p className="box-content">Mitra Penyedia Pinjaman Merchant Tokopedia</p>
-              <p className="box-detail" style={this.state.show===1 ? greenColor:greyColor}>Lihat Detail Produk</p>
+          <div
+            className="box"
+            style={this.state.show === 1 ? greenBorder : greyBorder}
+            onClick={(e) => this.changeProductDetail(e, 1)}
+          >
+            <div>
+              <i className="wrapper-icon box-icon mitratopper-icon"></i>
             </div>
-            <div className="box" style={this.state.show===2 ? greenBorder : greyBorder} onClick={(e) => this.changeProductDetail(e, 2)}>
-              <div>
-                <i className="wrapper-icon box-icon kartukredit-icon"></i>
-              </div>
-              <p className="box-title">Kartu Kredit</p>
-              <p className="box-content">Ajukan Kartu Kredit yang sesuai dengan kebutuhan</p>
-              <p className="box-detail" style={this.state.show===2 ? greenColor:greyColor}>Lihat Detail Produk</p>
+            <p className="box-title">Mitra Toppers</p>
+            <p className="box-content">
+              Mitra Penyedia Pinjaman Merchant Tokopedia
+            </p>
+            <p
+              className="box-detail"
+              style={this.state.show === 1 ? greenColor : greyColor}
+            >
+              Lihat Detail Produk
+            </p>
+          </div>
+          <div
+            className="box"
+            style={this.state.show === 2 ? greenBorder : greyBorder}
+            onClick={(e) => this.changeProductDetail(e, 2)}
+          >
+            <div>
+              <i className="wrapper-icon box-icon kartukredit-icon"></i>
             </div>
-            <div className="box" style={this.state.show===3 ? greenBorder : greyBorder} onClick={(e) => this.changeProductDetail(e, 3)}>
-              <div>
-                <i className="wrapper-icon box-icon pinjamanonline-icon"></i>
-              </div>
-              <p className="box-title">Pinjaman Online</p>
-              <p className="box-content">Ajukan Pinjaman tanpa Agunan yang Sesuai</p>
-              <p className="box-detail" style={this.state.show===3 ? greenColor:greyColor}>Lihat Detail Produk</p>
+            <p className="box-title">Kartu Kredit</p>
+            <p className="box-content">
+              Ajukan Kartu Kredit yang sesuai dengan kebutuhan
+            </p>
+            <p
+              className="box-detail"
+              style={this.state.show === 2 ? greenColor : greyColor}
+            >
+              Lihat Detail Produk
+            </p>
+          </div>
+          <div
+            className="box"
+            style={this.state.show === 3 ? greenBorder : greyBorder}
+            onClick={(e) => this.changeProductDetail(e, 3)}
+          >
+            <div>
+              <i className="wrapper-icon box-icon pinjamanonline-icon"></i>
             </div>
-            <div className="box" style={this.state.show===4 ? greenBorder : greyBorder} onClick={(e) => this.changeProductDetail(e, 4)}>
-              <div>
-                <i className="wrapper-icon box-icon asuransi-icon"></i>
-              </div>
-              <p className="box-title">Asuransi</p>
-              <p className="box-content">Ajukan Asuransi yang sesuai dengan kebutuhan</p>
-              <p className="box-detail" style={this.state.show===4 ? greenColor:greyColor}>Lihat Detail Produk</p>
+            <p className="box-title">Pinjaman Online</p>
+            <p className="box-content">
+              Ajukan Pinjaman tanpa Agunan yang Sesuai
+            </p>
+            <p
+              className="box-detail"
+              style={this.state.show === 3 ? greenColor : greyColor}
+            >
+              Lihat Detail Produk
+            </p>
+          </div>
+          <div
+            className="box"
+            style={this.state.show === 4 ? greenBorder : greyBorder}
+            onClick={(e) => this.changeProductDetail(e, 4)}
+          >
+            <div>
+              <i className="wrapper-icon box-icon asuransi-icon"></i>
             </div>
+            <p className="box-title">Asuransi</p>
+            <p className="box-content">
+              Ajukan Asuransi yang sesuai dengan kebutuhan
+            </p>
+            <p
+              className="box-detail"
+              style={this.state.show === 4 ? greenColor : greyColor}
+            >
+              Lihat Detail Produk
+            </p>
+          </div>
         </div>
 
-        <ProductDetail isComponent={this.state.show}/>
-
+        <ProductDetail isComponent={this.state.show} />
       </div>
     );
   }
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,104 +1,114 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ProductDetail extends Component {
-
-    render(){   
-        if(this.props.isComponent===1){
-            return(
-                <div className="row product-detail">
-                    <div className="col-lg-8 col-md-6">
-                        <h3 className="product-detail-title">Mitra Toppers</h3>
-                        <p className="product-detail-content">
-                        Mitra toppers adalah program kerja sama Tokopedia dengan penyedia pinjaman yang bertujuan memudahkan 
-                        Merchant Tokopedia untuk mendapatkan modal bagi pengembangan bisnis.
-                        </p>
-                        <button className="btn btn-sm btn-success btn-product-detail">Ajukan Pinjaman</button>
-                    </div>
-                    <div className="col-lg-4 col-md-6 left-detail-block">
-                        <h4>Benefit Mitra Toppers</h4>
-                        <ul>
-                        <li> Data Pendukung</li>
-                        <li> Banyak Pilihan Mitra</li>
-                        <li> Menambah Stok dan Varian Produk</li>
-                        <li> Menambah Karyawan</li>
-                        <li> Meningkatkan Penjualan lewat Promosi</li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        }else if(this.props.isComponent===2){
-            return(
-                <div className="row product-detail">
-                    <div className="col-lg-8 col-md-6">
-                        <h3 className="product-detail-title">Kartu Kredit</h3>
-                        <p className="product-detail-content">
-                        Temukan berbagai jenis kartu kredit dari bank-bank besar di Indonesia, dan nikmati beragam penawaran menariknya. Anda juga dapat membandingkan 
-                        beberapa jenis kartu kredit sekaligus di Tokopedia, ajukan kartu kredit pastinya jadi lebih mudah dan cepat.
-                        </p>
-                        <button className="btn btn-sm btn-success btn-product-detail">Ajukan Kartu Kredit</button>
-                    </div>
-                    <div className="col-lg-4 col-md-6 left-detail-block">
-                        <h4>Benefit Kartu Kredit</h4>
-                        <ul>
-                        <li> Pengajuan Online Kapan & Di Mana Saja</li>
-                        <li> Proses Cepat, Mudah & Aman</li>
-                        <li> Tanpa Dipungut Biaya</li>
-                        <li> Kerahasiaan Data Terjamin</li>
-                        <li> Nikmati Berbagai Promo Menarik</li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        }else if(this.props.isComponent===3){
-            return(
-                <div className="row product-detail">
-                    <div className="col-lg-8 col-md-6">
-                        <h3 className="product-detail-title">Pinjaman Online</h3>
-                        <p className="product-detail-content">
-                        Pinjaman Personal adalah program kerjasama Tokopedia dengan partner penyedia pinjaman yang bertujuan 
-                        memudahkan pengguna Tokopedia untuk mendapatkan pinjaman tanpa agunan yang sesuai.
-                        </p>
-                        <button className="btn btn-sm btn-success btn-product-detail">Ajukan Pinjaman</button>
-                    </div>
-                    <div className="col-lg-4 col-md-6 left-detail-block">
-                        <h4>Benefit Pinjaman Online</h4>
-                        <ul>
-                        <li> Banyak Pilihan Mitra Pinjaman</li>
-                        <li> Proses Cepat dan Mudah</li>
-                        <li> Pilih dan Bandingkan Pinjaman Sesuai Kebutuhan</li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        }else if(this.props.isComponent===4){
-            return(
-                <div className="row product-detail">
-                    <div className="col-lg-8 col-md-6">
-                        <h3 className="product-detail-title">Asuransi</h3>
-                        <p className="product-detail-content">
-                        Bingung dengan penawaran asuransi yang begitu beragam? Pelajari lebih lanjut berbagai macam jenis asuransi yang sesuai 
-                        dengan kebutuhan Anda hanya di Tokopedia. Pengajuan aplikasi relatif lebih cepat dan lebih mudah.
-                        </p>
-                        <button className="btn btn-sm btn-success btn-product-detail">Ajukan Asuransi</button>
-                    </div>
-                    <div className="col-lg-4 col-md-6 left-detail-block">
-                        <h4>Benefit Asuransi</h4>
-                        <ul>
-                        <li> Proses Mudah & Cepat</li>
-                        <li> Beragam Pilihan</li>
-                        <li> Premi Fleksibel</li>
-                        </ul>
-                    </div>
-                </div>
-            );
-        }else{
-            return(
-                null
-            );
-        }
+  render() {
+    if (this.props.isComponent === 1) {
+      return (
+        <div className="row product-detail">
+          <div className="col-lg-8 col-md-6">
+            <h3 className="product-detail-title">Mitra Toppers</h3>
+            <p className="product-detail-content">
+              Mitra toppers adalah program kerja sama Tokopedia dengan penyedia
+              pinjaman yang bertujuan memudahkan Merchant Tokopedia untuk
+              mendapatkan modal bagi pengembangan bisnis.
+            </p>
+            <button className="btn btn-sm btn-success btn-product-detail">
+              Ajukan Pinjaman
+            </button>
+          </div>
+          <div className="col-lg-4 col-md-6 left-detail-block">
+            <h4>Benefit Mitra Toppers</h4>
+            <ul>
+              <li> Data Pendukung</li>
+              <li> Banyak Pilihan Mitra</li>
+              <li> Menambah Stok dan Varian Produk</li>
+              <li> Menambah Karyawan</li>
+              <li> Meningkatkan Penjualan lewat Promosi</li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else if (this.props.isComponent === 2) {
+      return (
+        <div className="row product-detail">
+          <div className="col-lg-8 col-md-6">
+            <h3 className="product-detail-title">Kartu Kredit</h3>
+            <p className="product-detail-content">
+              Temukan berbagai jenis kartu kredit dari bank-bank besar di
+              Indonesia, dan nikmati beragam penawaran menariknya. Anda juga
+              dapat membandingkan beberapa jenis kartu kredit sekaligus di
+              Tokopedia, ajukan kartu kredit pastinya jadi lebih mudah dan
+              cepat.
+            </p>
+            <button className="btn btn-sm btn-success btn-product-detail">
+              Ajukan Kartu Kredit
+            </button>
+          </div>
+          <div className="col-lg-4 col-md-6 left-detail-block">
+            <h4>Benefit Kartu Kredit</h4>
+            <ul>
+              <li> Pengajuan Online Kapan & Di Mana Saja</li>
+              <li> Proses Cepat, Mudah & Aman</li>
+              <li> Tanpa Dipungut Biaya</li>
+              <li> Kerahasiaan Data Terjamin</li>
+              <li> Nikmati Berbagai Promo Menarik</li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else if (this.props.isComponent === 3) {
+      return (
+        <div className="row product-detail">
+          <div className="col-lg-8 col-md-6">
+            <h3 className="product-detail-title">Pinjaman Online</h3>
+            <p className="product-detail-content">
+              Pinjaman Personal adalah program kerjasama Tokopedia dengan
+              partner penyedia pinjaman yang bertujuan memudahkan pengguna
+              Tokopedia untuk mendapatkan pinjaman tanpa agunan yang sesuai.
+            </p>
+            <button className="btn btn-sm btn-success btn-product-detail">
+              Ajukan Pinjaman
+            </button>
+          </div>
+          <div className="col-lg-4 col-md-6 left-detail-block">
+            <h4>Benefit Pinjaman Online</h4>
+            <ul>
+              <li> Banyak Pilihan Mitra Pinjaman</li>
+              <li> Proses Cepat dan Mudah</li>
+              <li> Pilih dan Bandingkan Pinjaman Sesuai Kebutuhan</li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else if (this.props.isComponent === 4) {
+      return (
+        <div className="row product-detail">
+          <div className="col-lg-8 col-md-6">
+            <h3 className="product-detail-title">Asuransi</h3>
+            <p className="product-detail-content">
+              Bingung dengan penawaran asuransi yang begitu beragam? Pelajari
+              lebih lanjut berbagai macam jenis asuransi yang sesuai dengan
+              kebutuhan Anda hanya di Tokopedia. Pengajuan aplikasi relatif
+              lebih cepat dan lebih mudah.
+            </p>
+            <button className="btn btn-sm btn-success btn-product-detail">
+              Ajukan Asuransi
+            </button>
+          </div>
+          <div className="col-lg-4 col-md-6 left-detail-block">
+            <h4>Benefit Asuransi</h4>
+            <ul>
+              <li> Proses Mudah & Cepat</li>
+              <li> Beragam Pilihan</li>
+              <li> Premi Fleksibel</li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
     }
-
-
+  }
 }
 
 export default ProductDetail;
